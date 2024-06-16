3d2468b4bc032dafddc8c3204cead4706da513c4
dsomlee <165112600+dsomlee@users.noreply.github.com>
2024-06-16 14:27:40 +0900

    Add files via upload

--- app1.py
+++ app1.py
@@ -1,1 +1,6 @@
-
+import streamlit as st
+st.title('안녕하세요')
+st.title(':sparkles:' '간단한 설문 조사를 해보겠습니다')
+st.header('여러분의 요즘 관심사는 무엇인가요?')
+st.caption('취미 생활과 좋아하는 음식 등 여러분이 좋아하는 것이 무엇인지 알려주세요')
+st.markdown('**반가워요:blush:**')
